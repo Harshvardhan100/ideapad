46bd31aefd4f0fd95e4866e27643f70d68bc583e
Harshvardhan Prajapati <105856460+harshvardhanprajapati@users.noreply.github.com>
2022-09-21 01:18:21 +0530

    Create exception_class.java

---
+++ exception_class.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class myException extends Exception{
+    public String toString(){
+        return super.toString()+"im to Stering()";
+    }
+    public String getMessage(){
+return super.getMessage()+"im getmessage()";
+    }
+}
+public class exception_class{
+
+    public static void main(String[] args) {
+
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+
+if(a<9){
+    try{
+       // throw new myException();
+        throw new ArithmeticException("this is arithemetic exception");
+
+    }
+
+    catch (Exception e){
+        System.out.println(e.getMessage());
+        System.out.println(e.toString());
+        e.printStackTrace();
+        System.out.println("finised");
+    }
+}
+
+        System.out.println("yes finised");
+
+
+
+
+    }
+}
