9cbb511983d630dbf68449b365410ad1d15d8a48
Harshvardhan Prajapati <105856460+Harshvardhan100@users.noreply.github.com>
2022-09-25 23:18:34 +0530

    Create cylender_class.java

---
+++ cylender_class.java
@@ -0,0 +1,10 @@
+public class cylender_using_constructer {
+    public cylender_using_constructer() {
+    }
+
+    public static void main(String[] args) {
+        cylenderr non = new cylenderr(6, 5);
+        System.out.println(non.surcufrence());
+        System.out.println(non.volume());
+    }
+}
